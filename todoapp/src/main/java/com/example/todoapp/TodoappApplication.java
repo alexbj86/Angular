df4bdaef19d16a70bfd9d3cef7e96b6826706d80
Alessandro Buttarazzi <jalexb86@gmail.com>
2017-12-29 12:31:30 +0100

    cambiato nome package

--- todoapp/src/main/java/com/example/todoapp/TodoappApplication.java
+++ todoapp/src/main/java/com/example/todoapp/TodoappApplication.java
@@ -1,4 +1,4 @@
-package com.todoapp.todoapp;
+package com.example.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
