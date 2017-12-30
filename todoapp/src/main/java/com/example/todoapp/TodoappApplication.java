52941dd2eacb159b830bfab501696363d85b2b5e
alessandro <alessandro@alessandro-Inspiron-5721>
2017-12-30 12:31:03 +0100

    vv

--- todoapp/src/main/java/com/example/todoapp/TodoappApplication.java
+++ todoapp/src/main/java/com/example/todoapp/TodoappApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoappApplication.class, args);
+		SpringApplication.run(TodoappApplication.class, args);   
 	}
 }
