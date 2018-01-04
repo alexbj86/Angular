a1cb3214e8e3ea58ac442a62320ab0ad58385fb4
Alessandro <jalexb86@gmail.com>
2018-01-04 18:23:34 +0100

    aggiunto object customer e modifica appmodule

--- Angular4MongoDB/src/app/app.module.ts
+++ Angular4MongoDB/src/app/app.module.ts
@@ -1,27 +1,32 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+
 import { CustomersListComponent } from './customers/customers-list/customers-list.component';
-import { CustomersCreateComponent } from './customers/customers-create/customers-create.component';
 import { CustomerDetailsComponent } from './customers/customer-details/customer-details.component';
 import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
 
+import { CustomerService } from './customers/customer.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomersListComponent,
-    CustomersCreateComponent,
     CustomerDetailsComponent,
     CreateCustomerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Angular4MongoDB/src/app/customers/customer.service.ts
+++ Angular4MongoDB/src/app/customers/customer.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Customer } from './customer';
 
 @Injectable()
 export class CustomerService {
+
+  private baseUrl = 'http://localhost:8080/api/customers';
 
   constructor() { }
 
