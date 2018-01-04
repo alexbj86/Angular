cd36c3acae71af9ed73766f5bb3b45083f0879a0
Alessandro <jalexb86@gmail.com>
2018-01-04 18:32:31 +0100

    aggiunti metodi customer.service

--- Angular4MongoDB/src/app/customers/customer.service.ts
+++ Angular4MongoDB/src/app/customers/customer.service.ts
@@ -3,12 +3,36 @@ import { HttpClient, HttpRequest} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 import { Customer } from './customer';
+import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Injectable()
 export class CustomerService {
 
   private baseUrl = 'http://localhost:8080/api/customers';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  getCustomer(id:string): Observable<Object> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  getCustomersList(query = {}): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+  createCustomer(customer: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}` + `/create`, customer);
+  }
+
+  updateCustomer(id: string, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
+  }
+
+  deleteCustomer(id: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, {responseType: 'text'});
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(`${this.baseUrl}` + `/delete`, {responseType: 'text'});
+  }
 }
