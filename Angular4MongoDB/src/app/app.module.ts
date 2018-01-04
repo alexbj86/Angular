db22f5d9b830db915899bd5782605d0d39ebcb4c
alessandro <alessandro@alessandro-Inspiron-5721>
2018-01-04 11:28:58 +0100

    create component customers

--- Angular4MongoDB/src/app/app.module.ts
+++ Angular4MongoDB/src/app/app.module.ts
@@ -1,13 +1,22 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CustomersListComponent } from './customers/customers-list/customers-list.component';
+import { CustomersCreateComponent } from './customers/customers-create/customers-create.component';
+import { CustomerDetailsComponent } from './customers/customer-details/customer-details.component';
+import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersListComponent,
+    CustomersCreateComponent,
+    CustomerDetailsComponent,
+    CreateCustomerComponent
   ],
   imports: [
     BrowserModule
