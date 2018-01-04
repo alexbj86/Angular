f3f5d47d264837a0dc6c93f85f02a34f3efb04dd
Alessandro <jalexb86@gmail.com>
2018-01-04 21:43:18 +0100

    customer details

--- Angular4MongoDB/src/app/customers/customer-details/customer-details.component.ts
+++ Angular4MongoDB/src/app/customers/customer-details/customer-details.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { CustomerService } from '../customer.service';
+import { Customer} from '../customer';
+
+import { CustomersListComponent } from '../customers-list/customers-list.component';
 
 @Component({
   selector: 'app-customer-details',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() customer: Customer;
+
+  constructor(private customerService: CustomerService, private listComponent: CustomersListComponent) { }
 
   ngOnInit() {
+  }
+
+  updateActive(isActive: boolean){
+    this.customerService.updateCustomer(this.customer.id, {name: this.customer.name,
+                                                           age: this.customer.age, 
+                                                           active: isActive}).subscribe(data => {
+                                                             console.log(data);
+                                                             this.customer = data as Customer;
+                                                           },
+                                                          error => console.log(error));
+  }
+
+  deleteCustomer(){
+    this.customerService.deleteCustomer(this.customer.id).subscribe(data => {
+                                                                  console.log(data);
+                                                                  this.listComponent.reloadData();
+                                                                },
+                                                              error => console.log(error));
   }
 
 }
