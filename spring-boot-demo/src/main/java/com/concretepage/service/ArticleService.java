56fba8f525d991004dc6c140e0b9413f4a2b547b
alessandro <alessandro@alessandro-Inspiron-5721>
2018-02-05 22:19:44 +0100

    bug fix

--- spring-boot-demo/src/main/java/com/concretepage/service/ArticleService.java
+++ spring-boot-demo/src/main/java/com/concretepage/service/ArticleService.java
@@ -3,10 +3,12 @@ package com.concretepage.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.concretepage.dao.IArticleDAO;
 import com.concretepage.entity.Article;
 
+@Service
 public class ArticleService implements IArticleService {
 	
 	private @Autowired IArticleDAO articleDAO;
@@ -22,12 +24,12 @@ public class ArticleService implements IArticleService {
 	}
 
 	@Override
-	public boolean createArticle(Article article) {
+	public synchronized boolean createArticle(Article article) {
 		if(articleDAO.articleExists(article.getTitle(), article.getCategory())) {
 			return false;
 		}else {
 			articleDAO.createArticle(article);
-			return false;
+			return true;
 		}
 	}
 
