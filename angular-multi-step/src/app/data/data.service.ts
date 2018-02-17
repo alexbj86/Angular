34198b3caf929e992914ffd37735385b9e2ff91a
jalexb86 <jalexb86@gmail.com>
2018-02-17 19:00:08 +0100

    init data.service

--- angular-multi-step/src/app/data/data.service.ts
+++ angular-multi-step/src/app/data/data.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 
+import { FormData, Personal, Address } from './formData.model';
+
 @Injectable()
 export class DataService {
 
   constructor() { }
-
+    
 }
