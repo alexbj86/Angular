60d3267bdbdac616005d8676c1959d1b0dafef1b
Unknown <jalexb86@gmail.com>
2018-09-01 17:16:25 +0200

    fabio biondi's app angular smartphone list

---
+++ angular-smartphone-app/src/model/device.ts
@@ -0,0 +1,11 @@
+export interface Device {
+
+  // '?' indica che la proprietà è facoltativa
+  id?: number;
+  label?: string;
+  os?: string;
+  price?: number;
+  memory?: number;
+  rate?: number;
+  desc?: string;
+}
