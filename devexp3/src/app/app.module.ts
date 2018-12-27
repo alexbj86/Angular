6e925c7c6cb9de92f73aacd8e986f78c90f2a250
Unknown <jalexb86@gmail.com>
2018-12-27 14:57:56 +0100

    transclusion

--- devexp3/src/app/app.module.ts
+++ devexp3/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import {UtenteComponent} from "./utente/utente.component";
 import { UppercaserComponent } from './uppercaser/uppercaser.component';
 import { LowercaserComponent } from './lowercaser/lowercaser.component';
+import { PanelComponent } from './panel/panel.component';
+import { Panel2Component } from './panel2/panel2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UtenteComponent,
     UppercaserComponent,
-    LowercaserComponent
+    LowercaserComponent,
+    PanelComponent,
+    Panel2Component
   ],
   imports: [
     BrowserModule,
